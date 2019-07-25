c7822e96eb6e99d855ac9bd31ea504d9f67b5034
pulling-star <milanjackson426@gmail.com>
2019-07-25 14:45:56 +0200

    CSS fix for IE.

--- PluginWfBootstrapjs.php
+++ PluginWfBootstrapjs.php
@@ -1,7 +1,18 @@
 <?php
 class PluginWfBootstrapjs{
   public static function widget_include(){
-    $element = wfDocument::createHtmlElement('script', null, array('src' => '/plugin/wf/bootstrapjs/include.js?t='.wfFilesystem::getFiletime(wfArray::get($GLOBALS, 'sys/web_dir').'/plugin/wf/bootstrapjs/include.js')));
-    wfDocument::renderElement(array($element));
+    $element = array();
+    /**
+     * 
+     */
+    $element[] = wfDocument::createHtmlElement('script', null, array('src' => '/plugin/wf/bootstrapjs/include.js?t='.wfFilesystem::getFiletime(wfArray::get($GLOBALS, 'sys/web_dir').'/plugin/wf/bootstrapjs/include.js')));
+    /**
+     * IE fix.
+     */
+    $element[] = wfDocument::createHtmlElement('style', ".modal-content{overflow:auto}");
+    /**
+     * 
+     */
+    wfDocument::renderElement($element);
   }
 }
