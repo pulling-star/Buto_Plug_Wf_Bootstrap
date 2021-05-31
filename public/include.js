3afa78c7b205e17f753adbcd83a1c54d1f35f3b7
pulling-star <milanjackson426@gmail.com>
2021-05-31 15:18:13 +0200

    1.7.1

--- public/include.js
+++ public/include.js
@@ -82,8 +82,8 @@ function plugin_wf_bootstrapjs(){
                       attribute: {class: 'modal-header'},
                       innerHTML: [
                         {type: 'h4', attribute: {class: 'modal-title', onclick: "if(typeof PluginWfAjax == 'object'){PluginWfAjax.update('"+data.id+'_body'+"');}"}, innerHTML: data.label},
-                        {type: 'button', attribute: {type: 'button', class: 'close', style: btn_reload_style, onclick: "if(typeof PluginWfAjax == 'object'){PluginWfAjax.update('"+data.id+'_body'+"');}"}, innerHTML: [{type: 'img', innerHTML: null, attribute: {src: 'http://localhost/plugin/icons/octicons/build/svg/sync.svg'}}] },
-                        {type: 'button', attribute: {type: 'button', class: 'close', style: 'margin-left:0px', 'data-dismiss': 'modal', id: data.id+'_modal_dismiss'}, innerHTML: [{type: 'img', innerHTML: null, attribute: {src: 'http://localhost/plugin/icons/octicons/build/svg/x.svg'}}]} 
+                        {type: 'button', attribute: {type: 'button', class: 'close', style: btn_reload_style, onclick: "if(typeof PluginWfAjax == 'object'){PluginWfAjax.update('"+data.id+'_body'+"');}"}, innerHTML: [{type: 'img', innerHTML: null, attribute: {src: '/plugin/icons/octicons/build/svg/sync.svg'}}] },
+                        {type: 'button', attribute: {type: 'button', class: 'close', style: 'margin-left:0px', 'data-dismiss': 'modal', id: data.id+'_modal_dismiss'}, innerHTML: [{type: 'img', innerHTML: null, attribute: {src: '/plugin/icons/octicons/build/svg/x.svg'}}]} 
                       ]
                     },
                     {type: 'div', attribute: {class: 'modal-body', id: data.id+'_body'}, innerHTML: data.content},
